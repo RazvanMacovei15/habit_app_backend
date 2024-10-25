e442354279dc4496417867e5c49b51eb6c8b1d48
Razvan Macovei <razvanmc15@gmail.com>
2024-10-25 11:23:21 +0300

    app launch

---
+++ src/main/java/maco/hobby_backend/HobbyBackendApplication.java
@@ -0,0 +1,13 @@
+package maco.hobby_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbyBackendApplication.class, args);
+	}
+
+}
